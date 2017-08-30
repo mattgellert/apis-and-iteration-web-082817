7cb37c88884d35f6c3d9ac5c11f16e7c96f7767d
mattgellert <mattgellert@gmail.com>
2017-08-29 20:18:54 -0400

    Done.

--- bin/run.rb
+++ bin/run.rb
@@ -2,7 +2,6 @@
 
 require_relative "../lib/api_communicator.rb"
 require_relative "../lib/command_line_interface.rb"
-require 'benchmark'
 
 welcome
 character = get_character_from_user
@@ -11,4 +10,5 @@ x = show_character_movies(character)
 until x != []
     puts "That is not a valid character. Please try again"
     character = get_character_from_user
+    x = show_character_movies(character)
 end
